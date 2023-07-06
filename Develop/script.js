903bc3fb111d058e9dab5071f103ce5410a96358
Adrian Patton <adrian.j.patton33@gmail.com>
2023-07-06 18:50:14 -0400

    modified code for challenge

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,39 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// this is the date at the top of the page.
+   var today = dayjs();
+  $('#1a').text(today.format('dddd, MMM D, YYYY'));
+
+  // this saves the text in the text box.
+  $(".saveBtn").click(function funky() {
+  var savedDiv = $(this).parent().attr("id")
+  var divText = $(this).parent().children("textarea").val();
+  localStorage.setItem(savedDiv, divText)
+});
+
+// saves the value inputed to each textbox.
+$(".description").each(function() {
+  var dynDiv = $(this).parent().attr("id")
+  $(this).text(localStorage.getItem(dynDiv))
+});
+
+// changes the class to past, present, and future.
+  var currentHour = today.hour() 
+
+  $(".time-block").each(function () {
+      let timeDiv = $(this).attr('id')
+      if (currentHour == timeDiv) {
+      $(this).addClass("present")
+  
+      } else if (currentHour < timeDiv) {
+        $(this).addClass("future")
+  
+      } else if (currentHour > timeDiv) {
+       $(this).addClass("past")
+      }
+    })
+  
+
+
+  
 });
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Work Day Scheduler Starter Code
+# apatton33-calender-application
+
+## Description
+    I was tasked through the acceptance criteria to modify an application that saves events for each hour.
+    It needed the current date added to the top of the page.
+    It needed time blocks for the business hours through the day.
+    It presents them in past, present, and future.
+    I can enter and event into the block and save it.
+    If you refresh the page it showed it stored until deleted and saved again.
+    
+
+## Usage
+    I can enter and event into the block and save it.
+    If you refresh the page it showed it stored until deleted and saved again.
+
+
+## Deployment site
